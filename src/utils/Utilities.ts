a4b34151684e46036791fa08359a5e795f18f093
Sebastian Blanco <sebastianblancogonz@gmail.com>
2024-01-14 15:12:55 +0100

    :sparkles: Add pie chart to expenses page¨

--- src/utils/Utilities.ts
+++ src/utils/Utilities.ts
@@ -1,4 +1,4 @@
-import type { Expense } from '../types/ExpenseTrackerTypes'
+import type { Expense, ChartData } from '../types/ExpenseTrackerTypes'
 
 export function getMonthName(date: Date) {
 	return date.toLocaleDateString('default', { month: 'long' }).substring(0, 3).toUpperCase()
@@ -29,13 +29,33 @@ export function categoryEmoji(category: string) {
 	}
 }
 
-export function buildExpensesChartData(expenses: Expense[]) {
-	var data: Map<string, number>;
+export function buildExpensesChartData(expenses: Expense[]): ChartData {
+	var data: Map<string, number> = new Map()
 	expenses.map((expense) => {
-		if (!data.has(expense.category)) {
+		let category = expense.category
+		if (!data.has(category)) {
 			data.set(expense.category, expense.amount)
 		} else {
-			data.set(expense.category, data.get(expense.category) + expense.amount)
+			let mapAmount = data.get(category)
+			data.set(expense.category, mapAmount != undefined ? +expense.amount : expense.amount)
 		}
 	})
+
+	return {
+		type: 'pie',
+		data: {
+			labels: [...data.keys()],
+			datasets: [
+				{
+					label: 'Expenses chart',
+					data: [...data.values()],
+					hoverOfsset: 5,
+					backgroundColor: ['rgb(239,183,178)']
+				}
+			]
+		},
+		options: {
+			responsive: false
+		}
+	}
 }
