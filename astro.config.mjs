109a4c77bcf6b34599d8f669cf0c277803a98834
Sebastian Blanco <sebastianblancogonz@gmail.com>
2024-01-19 18:33:25 +0100

    Add ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,19 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
 
-import node from "@astrojs/node";
+import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
-	output: "hybrid",
-  integrations: [tailwind(), react({
-    include: ['**/react/*']
-  })],
-  adapter: node({
-    mode: "standalone"
-  })
-});
+	output: 'server',
+	integrations: [
+		tailwind(),
+		react({
+			include: ['**/react/*']
+		})
+	],
+	adapter: node({
+		mode: 'standalone'
+	})
+})
